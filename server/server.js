965643c6a61dc9a24be806be6fce68ad1d08e999
somu878 <somumydhili19@gmail.com>
2024-01-28 18:03:54 +0530

    origin env added @server

--- server/server.js
+++ server/server.js
@@ -7,10 +7,11 @@ const authrouter = require("./routes/authRoute");
 const jobrouter = require("./routes/jobRoute");
 const cookieParser = require("cookie-parser");
 var cors = require("cors");
+const origin = process.env.ORIGIN;
 app.use(cookieParser());
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", origin);
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   res.header("Access-Control-Allow-Credentials", "true");
