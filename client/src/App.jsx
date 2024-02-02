b90c0efd31d6bc2152f21ba2ba5caec49baf75d0
somu878 <somumydhili19@gmail.com>
2024-02-02 20:53:10 +0530

    added lazy loading to all routes @client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,70 @@
-import "./App.css";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import PostJob from "./pages/postJob/PostJob";
-import JobDetail from "./pages/jobDetails/JobDetail";
-import Register from "./pages/registration/Register";
-import toast, { Toaster } from "react-hot-toast";
-import EditJob from "./pages/editJob/EditJob";
+import "./App.css";
+import { Toaster } from "react-hot-toast";
+import Loader from "./components/loader/Loader";
+
+const HomeLazy = lazy(() => import("./pages/home/Home"));
+const LoginLazy = lazy(() => import("./pages/login/Login"));
+const RegisterLazy = lazy(() => import("./pages/registration/Register"));
+const PostJobLazy = lazy(() => import("./pages/postJob/PostJob"));
+const JobDetailLazy = lazy(() => import("./pages/jobDetails/JobDetail"));
+const EditJobLazy = lazy(() => import("./pages/editJob/EditJob"));
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/addjob" element={<PostJob />} />
-        <Route path="/job-details/:id" element={<JobDetail />} />
-        <Route path="/editjob/:id" element={<EditJob />} />
+        <Route
+          path="/"
+          element={
+            <Suspense fallback={<Loader />}>
+              <HomeLazy />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <Suspense fallback={<Loader />}>
+              <LoginLazy />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Suspense fallback={<Loader />}>
+              <RegisterLazy />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/addjob"
+          element={
+            <Suspense fallback={<Loader />}>
+              <PostJobLazy />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/job-details/:id"
+          element={
+            <Suspense fallback={<Loader />}>
+              <JobDetailLazy />
+            </Suspense>
+          }
+        />
+        <Route
+          path="/editjob/:id"
+          element={
+            <Suspense fallback={<Loader />}>
+              <EditJobLazy />
+            </Suspense>
+          }
+        />
       </Routes>
-      <Toaster position="top-center" reverseOrder="false" />
+      <Toaster position="top-center" reverseOrder={false} />
     </BrowserRouter>
   );
 }
